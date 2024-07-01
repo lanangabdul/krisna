a3b6304fdbf0870ebeb0d92700eef158e95b5a98
unknown <jiwamantap502.com>
2024-07-02 00:01:03 +0700

    updatess2

--- routers/index.js
+++ routers/index.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const AuthRouter = require("./AuthRouter");
 
 router.use("/api/v1/auth", AuthRouter);
-router.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
 
 module.exports = router
