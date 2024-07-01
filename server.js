251f4f1922084b2e9669bb243fb06a9eb0f6039c
unknown <jiwamantap502.com>
2024-07-01 20:52:26 +0700

    updatess1

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const app = require("./index");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const port = 5000;
+const port =  process.env.PORT || 5000;
+const database = process.env.DATABASE_URI;
 
 
 mongoose
-.connect("mongodb+srv://setiawankrisna321:gM5UxUgtdpu4uX3a@cluster0.zccs2wk.mongodb.net/")
+.connect(database)
 .then(() => {
     console.log("Database Berhasil Terkoneksi");
   });
